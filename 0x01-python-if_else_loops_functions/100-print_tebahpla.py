649dd3e42398efc6584aa090635b0b472024390a
Malik <malikhussein229@gmail.com>
2024-01-02 16:37:10 -0800

    #

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(90, 64, -1):
     if i % 2 == 0:
-        print("{:s}".format(chr(i + 32)), end ='')
+        print("{:s}".format(chr(i + 32)), end='')
     else:
         print("{:s}".format(chr(i)), end='')
