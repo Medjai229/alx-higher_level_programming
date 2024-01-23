7bbf3e328b0b916e067a7227ffd44c38219687a2
Malik <malikhussein229@gmail.com>
2024-01-23 11:06:22 -0800

    #

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""square class."""
+
+
+class Square:
+    """square"""
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: side length of the square
+        """
+        self.__size = size
