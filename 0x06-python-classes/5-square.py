a03daa3359825d60c443492c7cd37a439bcd0a57
Malik <malikhussein229@gmail.com>
2024-01-23 11:37:32 -0800

    #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -40,6 +40,10 @@ class Square:
 
     def my_print(self):
         """prints a sqaure"""
+        if self.size == 0:
+            print()
+            return
+
         for i in range(self.size):
             for j in range(self.size):
                 print("#", end='')
