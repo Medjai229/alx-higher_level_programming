9ac3be91c8c69ad6199c5323dd4950e2af06338b
Malik <malikhussein229@gmail.com>
2024-01-29 09:59:15 -0800

    #

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+File: 0-rectangle.py
+Description: defines a class called Rectangle
+"""
+
+
+class Rectangle:
+    """
+    this is a class defination called Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        this method retrives the value of attribute width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        this method sets the value of the attribute width
+        """
+        if isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        this method retrives the value of attribute height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        this method sets the value of the attribute height
+        """
+        if isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
