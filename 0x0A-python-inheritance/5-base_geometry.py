71472140b6905c1a164530baf8c96bc2903d52fd
Malik <malikhussein229@gmail.com>
2024-02-05 07:16:29 -0800

    #

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+File: 5-base_geometry.py
+Description: This module contains one class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    an empty class
+    """
+    pass
