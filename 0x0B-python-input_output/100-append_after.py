6cba4148f09e04ae58817a5de2889cc65ec96ec8
Malik <malikhussein229@gmail.com>
2024-02-06 15:52:38 -0800

    #

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,7 +8,8 @@ Description: this module contains one function append_after
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    a function that inserts a line of text to a file, after each line containing a specific string
+    a function that inserts a line of text to a file
+    after each line containing a specific string
     """
     with open(filename, encoding="utf-8") as my_file:
         list_lines = my_file.readlines()
