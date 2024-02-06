12e104b1e836e8647a5e431e88f327a1bedd7c18
Malik <malikhussein229@gmail.com>
2024-02-06 15:51:49 -0800

    #

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+File: 100-append_after.py
+Description: this module contains one function append_after
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    a function that inserts a line of text to a file, after each line containing a specific string
+    """
+    with open(filename, encoding="utf-8") as my_file:
+        list_lines = my_file.readlines()
+        new_content = ""
+        for line in list_lines:
+            new_content += line
+            if search_string in line:
+                new_content += new_string
+
+    with open(filename, "w", encoding="utf-8") as my_file:
+        my_file.write(new_content)
