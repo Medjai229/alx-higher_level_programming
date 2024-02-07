0b983ece83c4bf521924a7d54b54526836c31fcf
Malik <malikhussein229@gmail.com>
2024-02-06 16:09:30 -0800

    #

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,8 +7,16 @@ Description: this script reads stdin line by line and computes metrics
 from sys import stdin
 
 
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0}
+status_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+        }
 
 total_size = i = 0
 
