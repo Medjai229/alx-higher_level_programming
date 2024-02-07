fe03a221bb7a9940f6af72123ba6a978b63d9316
Malik <malikhussein229@gmail.com>
2024-02-06 16:13:36 -0800

    #

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,14 +7,13 @@ Description: this script reads stdin line by line and computes metrics
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
+    """
+    a function that prints the stats
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
