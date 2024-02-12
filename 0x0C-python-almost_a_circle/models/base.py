0c9413825887e579e876281bf3fc552078706174
Malik <malikhussein229@gmail.com>
2024-02-12 12:24:15 -0800

    File to instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -68,3 +68,17 @@ class Base:
                 inst = cls(1, 2)
             inst.update(**dictionary)
             return inst
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        this method returns a list of instances
+        """
+        filename = f"{cls.__name__}.json"
+
+        try:
+            with open(filename, "r") as f:
+                list_dicts = cls.from_json_string(f.read())
+                return [cls.create(**dictionary) for dictionary in list_dicts]
+        except IOError:
+            return []
