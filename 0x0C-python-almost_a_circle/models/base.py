14bfe1694824b4baff9bcf01811491899735e41b
Malik <malikhussein229@gmail.com>
2024-02-12 14:01:22 -0800

    Manual Review

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -124,3 +124,46 @@ class Base:
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """
+        Draw all the Rectangles and Squares using Turtle graphics.
+
+        Args:
+        -   list_rectangles (list): A list of Rectangle instances.
+        -   list_squares (list): A list of Square instances.
+        """
+        # Create a turtle screen
+        screen = turtle.Screen()
+        screen.bgcolor("white")
+
+        # Create a turtle object
+        painter = turtle.Turtle()
+        painter.speed(1)
+        painter.pensize(2)
+        # draw the squares
+        for sq in list_squares:
+            print(sq)
+            painter.color("red")
+            painter.penup()
+            painter.goto(sq.x, sq.y)
+            painter.pendown()
+            for _ in range(4):
+                painter.forward(sq.size)
+                painter.left(90)
+
+        # draw the rectangles
+        for rect in list_rectangles:
+            print(rect)
+            painter.penup()
+            painter.goto(rect.x, rect.y)
+            painter.pendown()
+            for _ in range(2):
+                painter.color("indigo")
+                painter.forward(rect.width)
+                painter.left(90)
+                painter.forward(rect.height)
+                painter.left(90)
+
+        screen.exitonclick()
