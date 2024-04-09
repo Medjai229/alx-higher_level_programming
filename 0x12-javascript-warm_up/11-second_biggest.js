69b512d470179e0b38b80552ecfff59da3a738d6
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2024-04-09 05:31:16 +0200

    Fix Task#11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,8 @@ const argv = process.argv;
 const numArr = argv.splice(2).map(Number);
 
 if (numArr.length <= 1) {
-	console.log('0');
+  console.log('0');
 } else {
-	numArr.sort((a, b) => b - a);
-	console.log(numArr[1]);
+  numArr.sort((a, b) => b - a);
+  console.log(numArr[1]);
 }
