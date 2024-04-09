e2ab51e480c7cbf2c881a4a7e04ee88268a0c1fe
Malik <malikhussein229@gmail.com>
2024-04-08 19:26:02 -0700

    Fix Task#5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const argv = process.argv;
 
 if (isNaN(argv[2]) || argv[2] === undefined) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + parseInt(argv[2]));
+  console.log('My number: ' + parseInt(argv[2]));
 }
