9e53715dea657e6e81881a96efb79550aa25bae7
Malik <malikhussein229@gmail.com>
2024-04-08 19:21:01 -0700

    Fix Task#2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const argNum = process.argv.length;
 
 if (argNum === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argNum === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
