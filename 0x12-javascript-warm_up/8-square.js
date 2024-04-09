8b99c7f4d6ce0fa3805b1c81542c9fc57d0677d9
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2024-04-09 05:28:21 +0200

    Fix Task#8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,8 +5,8 @@ const argv = process.argv;
 if (isNaN(argv[2]) || argv[2] === undefined) {
   console.log('Missing size');
 } else {
-	const size = parseInt(argv[2]);
-	for (let i = 0; i < size; i++) {
-		console.log('X'.repeat(size));
-	}
+  const size = parseInt(argv[2]);
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
