f29d5c850cb6c128f71b7dff83dc8c84b5928297
Malik <malikhussein229@gmail.com>
2024-04-08 19:17:47 -0700

    Task#0:Task#6

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const cVar = 'C is fun';
+const pythonVar = 'Python is cool';
+const jsVar = 'JavaScript is amazing';
+console.log(cVar);
+console.log(pythonVar);
+console.log(jsVar);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+if (isNaN(argv[2]) || argv[2] === undefined) {
+	console.log('Not a number');
+} else {
+	console.log('My number: ' + parseInt(argv[2]));
+}
