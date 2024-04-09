1906308bc8b0fd90f490cca80ca74670eaf95424
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2024-04-09 05:27:09 +0200

    Fix Task#7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const argv = process.argv;
 
 if (isNaN(argv[2]) || argv[2] === undefined) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i < parseInt(argv[2]); i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    console.log('C is fun');
+    }
 }
