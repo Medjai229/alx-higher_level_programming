9576f7385aedb2fc087a307a88f6256c1161ab8a
Malik <malikhussein229@gmail.com>
2024-04-15 11:01:00 -0700

    Task#14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition of
+a State and an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    This class inherits from Base and link to MySQL table citites
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
