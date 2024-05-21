dbe6c7811401d7ddf522fcf7e6e522be7cad4e47
Malik <malikhussein229@gmail.com>
2024-05-22 01:09:17 +0300

    Task#5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (error == null) {
+  if (error === null) {
     fs.writeFile(process.argv[3], body, (err) => {
       if (err) console.log(error);
     });
