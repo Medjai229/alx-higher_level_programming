9b9cdab06f85fb55d46e22b65d62d469294b317a
Malik <malikhussein229@gmail.com>
2024-05-22 00:19:54 +0300

    Task#3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (err == null) {
+    const jsonData = JSON.parse(body);
+    console.log(jsonData.title);
+  }
+});
